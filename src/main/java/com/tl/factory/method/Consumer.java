6a9819d7b8b0a1ba08a9586a0711a397558b0f54
guijih <18300613195@163.com>
2020-03-01 00:11:11 +0800

    feat: 工厂方法模式添加注释 符合开闭原则

--- src/main/java/com/tl/factory/method/Consumer.java
+++ src/main/java/com/tl/factory/method/Consumer.java
@@ -1,13 +1,25 @@
 package com.tl.factory.method;
-
+/**@Description 工厂方法模式
+ * 在简答工厂模式的前提下 增加了工厂抽象
+ * 添加新的车时候添加新的工厂和车辆即可
+ * 符合开闭原则
+ * @ClassName Consumer
+ * @author wangwei
+ * @date 2020/3/1 0001 0:06
+ */
 public class Consumer {
     public static void main(String[] args) {
-        Car car = new WuLingFactory().getCar();
-        Car car1 = new TeslaFactory().getCar();
-        Car car2 = new MobilFactory().getCar();
-        car.name();
-        car1.name();
-        car2.name();
+
+
+        CarFactory factory=new MobilFactory();
+        factory.getCar().name();
+
+        CarFactory tesla=new TeslaFactory();
+        tesla.getCar().name();
+
+
+
+
 
 
     }
